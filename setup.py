2fa9fc0c5c96b6378102b71f8dfbd8e594d63699
Bogdan Tanygin <bogdan@quasar2195.home>
2024-02-05 16:55:33 +0100

    python version update

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ config = {
     'author_email': 'b.j.morgan@bath.ac.uk',
     'version': VERSION,
     'install_requires': open( 'requirements.txt' ).read(),
-    'python_requires': '>=3.5',
+    'python_requires': '>=3.12.1',
     'license': 'MIT',
     'packages': ['bsym', 'bsym.interface'],
     'scripts': [],
