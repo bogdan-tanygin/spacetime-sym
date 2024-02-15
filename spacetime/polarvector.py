644a475fd709e97ac849db2a75e0c6e75c83c40e
Bogdan Tanygin <bogdan@quasar2195.local>
2024-02-15 20:13:05 +0100

    Get rid of redundant classes

--- spacetime/polarvector.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-
-class PolarVector( np.array ):
-
-    class_str = 'PolarVector'
--- spacetime/spacetime.py
+++
@@ -1,6 +0,0 @@
-import warnings
-import sys
-
-warnings.warn( "\nYou are trying to import spacetime.spacetime, which is not compatible with this version of spacetime-sym." )
-
-sys.exit()
