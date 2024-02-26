4a4bdf0850b937c79a4ee5c7425ebbc8709020c4
Dr. Bogdan Tanygin <b.m.tanygin@gmail.com>
2024-02-26 18:55:20 +0100

    extra tests, pq refactoring

--- tests/unit_tests/test_physical_quantity.py
+++ tests/unit_tests/test_physical_quantity.py
@@ -18,8 +18,7 @@
 #
 
 import unittest
-from unittest.mock import Mock, patch
-from spacetime.physicalquantity import PhysicalQuantity
+from spacetime.physical_quantity import PhysicalQuantity
 import numpy as np
 
 class TestPhysicalQuantity( unittest.TestCase ):
@@ -30,7 +29,7 @@ class TestPhysicalQuantity( unittest.TestCase ):
         self.vector = [-1/2, 0.4, 637e-8]
 
     def test_reassign_cpt_dis( self ):
-        pq = Mock( spec = PhysicalQuantity )
+        pq = PhysicalQuantity()
         pq.dis = self.dis
         cpt_test = [
             {"C":-1, "P":-1, "T":-1},
@@ -44,27 +43,46 @@ class TestPhysicalQuantity( unittest.TestCase ):
                 self.assertEqual( pq.dis[key], cpt_test[i][key] )
     
     def test_add_new_dis_symmetry( self ):
-        pq = Mock( spec = PhysicalQuantity )
+        pq = PhysicalQuantity()
+        # default dis
         dis_test = self.dis
         pq.dis = dis_test
-        # mass inversion parity
+        # mass inversion dis
         pq.dis["M"] = -1
         # make sure that the CPT symmetry stay the same
         keys = dis_test.keys()
         for key in keys:
             self.assertEqual( pq.dis[key], dis_test[key] )
-        # test mass inversion parity
+        # test mass inversion dis
         self.assertEqual( pq.dis["M"], -1 )
+    
+    def test_assigning_dis_with_wrong_value( self ):
+        pq = PhysicalQuantity()
+        # new incorrect dis value
+        dis_test = {"X":-2}
+        with self.assertRaises( ValueError ):
+            # new incorrect dis setting
+            pq.dis = dis_test
+
+    def test_reassigning_dis_with_wrong_value( self ):
+        pq = PhysicalQuantity()
+        # default dis
+        pq.dis = self.dis
+        # new incorrect dis value
+        dis_test = {"X":-2}
+        with self.assertRaises( ValueError ):
+            # extra incorrect dis setting
+            pq.dis = dis_test
 
     def test_reassign_scalar_value( self ):
-        pq = Mock( spec = PhysicalQuantity )
+        pq = PhysicalQuantity()
         pq.value = self.scalar
         scalar_test = - np.sqrt(3.0)
         pq.value = scalar_test
         self.assertEqual( pq.value, scalar_test)
 
     def test_reassign_vector_value( self ):
-        pq = Mock( spec = PhysicalQuantity )
+        pq = PhysicalQuantity()
         pq.value = self.vector
         vector_test =[np.sqrt(3.0), np.exp(-2.3), 0]
         pq.value = vector_test
